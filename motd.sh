38aaa5acf192dac087d6b092937c0a2c9546d6a8
Gabriel Brown <gabriel.brown@rallyhealth.com>
2020-09-09 16:15:09 -0700

    weather

--- motd.sh
+++ motd.sh
@@ -1,5 +1,14 @@
-# Prints out ASCII art welcome message along with the current day of the week, month, day of the month and time
+# Message of the day. Yay ascii art!
+# 
+# CLI breakdown:
+#   - figlet -> makes ascii art out  of strings
+#   - lolcat -> makes things colorful
+#   - date -> gets the current date in the desired format
 figlet "Welcome, Gabe" | lolcat
 figlet $(date +'%A, %B %d') | lolcat
 figlet $(date +'%I : %M %p') | lolcat
 
+# Provides weather info. curl "wttr.in/:help" to see full usage.
+# Current iteration shows current weather for current location,
+# with as little fluff as possible. VPN screws up location tracking though.
+curl "wttr.in?0Q"
