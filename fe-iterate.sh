4e18000b0f4fa416fe640306867cced61ff5f216
Gabriel Brown <gabriel.brown.h@gmail.com>
2022-12-03 15:42:25 -0500

    remove old, unportable scripts

--- fe-iterate.sh
+++
@@ -1,4 +0,0 @@
-!#/bin/bash
-
-HUSKY_SKIP_HOOKS=1 git commit -a -m "iterating" --no-verify
-git push
--- ops-proxy-marathon-versions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Makes it easier to check the version of a component 
-# on different environments via ops proxy
-#
-# $1 -> environment, ie bluesteel.werally.in
-# $2 -> search string, ie settings
-
-curl "https://ops-proxy.$1/marathon/versions" -s | jq | grep $2
