1a5c826325a7c27293d4b35e723a6a7b02c15c5d
Gabriel Brown <gabriel.brown.h@gmail.com>
2022-12-03 15:38:20 -0500

    add intellij and git config to quickstart

--- quickstart.sh
+++ quickstart.sh
@@ -1,3 +1,13 @@
+echo "Welcome to Gabe's quickstart script!"
+echo "Usage: sh ./quickstart.sh [GIT_EMAIL]"
+
+GIT_EMAIL=$1
+
+if [-z $GIT_EMAIL]; then
+    echo "Must provide an email for git to use with commits."
+    exit 1
+fi
+
 echo "Installing many common dev dependencies for MacOS"
 
 echo "Installing standard MacOS command line tools..."
@@ -12,6 +22,9 @@ curl -s "https://get.sdkman.io" | bash
 echo "Installing VSCode (general purpose text editor)..."
 brew install --cask visual-studio-code
 
+echo "Installing IntelliJ (great IDE for Scala and Java)..."
+brew install --cask intellij-idea-ce
+
 echo "Installing Ammonite (Scala REPL)..."
 brew install ammonite-repl
 
@@ -20,3 +33,7 @@ brew install kubernetes-cli
 brew install kubectx
 brew install minikube
 brew install --cask lens
+
+echo "Setting up some standard configs..."
+echo "Updating ~/.gitconfig"
+cat ./git_config.txt > ~/.gitconfig
